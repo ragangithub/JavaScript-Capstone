8c036f7b4135c335918600e458dae960696ad52c
ragan <raganalemu3@gmail.com>
2023-05-24 12:20:15 +0300

    deploy page

--- src/test/getMovies.test.js
+++ src/test/getMovies.test.js
@@ -1,33 +1,30 @@
-import getMovies from "../modules/movieApi.js";
+import getMovies from '../modules/movieApi.js';
 
-global.fetch = jest.fn(() =>
-  Promise.resolve({
-    json: () =>
-      Promise.resolve([
-        {
-          show: {
-            id: 1,
-            url: "https://www.tvmaze.com/shows/230/go-on",
-            name: "First movie",
-            type: "Scripted",
-            language: "English",
-          },
-        },
-        {
-          show: {
-            id: 2,
-            url: "https://www.tvmaze.com/shows/228/last-resort",
-            name: "Last Movie",
-            type: "Scripted",
-            language: "English",
-          },
-        },
-      ]),
-  })
-);
+global.fetch = jest.fn(() => Promise.resolve({
+  json: () => Promise.resolve([
+    {
+      show: {
+        id: 1,
+        url: 'https://www.tvmaze.com/shows/230/go-on',
+        name: 'First movie',
+        type: 'Scripted',
+        language: 'English',
+      },
+    },
+    {
+      show: {
+        id: 2,
+        url: 'https://www.tvmaze.com/shows/228/last-resort',
+        name: 'Last Movie',
+        type: 'Scripted',
+        language: 'English',
+      },
+    },
+  ]),
+}));
 
-describe("items counter tests using Jest", () => {
-  test("test getMovies function to the result", async () => {
+describe('items counter tests using Jest', () => {
+  test('test getMovies function to the result', async () => {
     const response = await getMovies();
 
     expect(response.length).toBe(2);
